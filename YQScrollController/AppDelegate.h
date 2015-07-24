6544d9166bba19495c14bb50587e7cc1445dbe25
yuyedaidao <wyqpadding@gmail.com>
2015-07-24 19:31:19 +0800

    Initial Commit

---
+++ YQScrollController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YQScrollController
+//
+//  Created by Wang on 15/7/24.
+//  Copyright (c) 2015年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YQScrollController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQScrollController
+//
+//  Created by Wang on 15/7/24.
+//  Copyright (c) 2015年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
